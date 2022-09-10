96aa3b9618f45d016c9ef773a354e87c33bfd31d
stuartchaney <stuartchaney22@gmail.com>
2022-09-10 13:37:43 -0700

    Initial Commit

---
+++ spec/omniauth/gorgias/oauth2_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Omniauth::Gorgias::Oauth2 do
+  it "has a version number" do
+    expect(Omniauth::Gorgias::Oauth2::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
